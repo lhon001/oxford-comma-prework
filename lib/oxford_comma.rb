9999710fe895832d7866e1353d83555693633e74
github username <github email address>
2018-04-02 20:39:56 -0700

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,17 @@
 def oxford_comma(array)
-
+  result = ""
+  if array.length > 2
+    array.each_with_index do |word, idx|
+      if idx == array.length - 1
+        result += "and #{word}"
+      else
+        result += "#{word}, "
+      end
+    end
+  elsif array.length == 2
+    result << "#{array[0]} and #{array[1]}"
+  else
+    result << array[0]
+  end
+  result
 end
